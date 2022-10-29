52a208e4c9f92a4a51eeb2eaa4d4d7d16e959d7a
prayugo <pray.arsian24@gmai.com>
2022-10-29 12:33:49 +0700

    penambahan link di navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,15 @@
 import React from "react";
 import '../assets/css/navbar.css'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return(
         <div className="nav">
             <h1>Kode Akadmeia</h1>
             <ul className="myUl">
-                <li>Home</li>
-                <li>About</li>
-                <li>Kontak</li>
+                <li><Link to="/" className="link">Home</Link></li>
+                <li><Link to="/about" className="link">About</Link></li>
+                <li><Link to="/contact" className="link">Contact</Link></li>
             </ul>
         </div>
     )
