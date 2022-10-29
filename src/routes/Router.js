c98c5d24bd818f8fc6c89012c9c53295ad6f4e2d
prayugo <pray.arsian24@gmai.com>
2022-10-29 13:43:53 +0700

    penamhana page not found

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -3,16 +3,19 @@ import Home from "../views/Home";
 import About from "../views/About";
 import Contact from "../views/Contact";
 import Detail from "../views/Detail";
-import { Route,Routes } from 'react-router-dom'
+import NotFound from "../views/NotFound";
+import { Route, Routes } from 'react-router-dom'
 
 const Router = () =>{
     return(
       <React.Fragment>
         <Routes>
-          <Route path='/' element={<Home/>} exact />
-          <Route path='/about' element={<About/>} exact/>
-          <Route path='/contact' element={<Contact/>} exact/>
-          <Route path='/detail/:id' element={<Detail/>} exact/>
+          
+            <Route path='/' element={<Home/>} exact />
+            <Route path='/about' element={<About/>} exact/>
+            <Route path='/contact' element={<Contact/>} exact/>
+            <Route path='/detail/:id' element={<Detail/>} exact/>
+            <Route path='*' element={<NotFound />} />
         </Routes>
       </React.Fragment>
     )
---
+++ src/views/NotFound.js
@@ -0,0 +1,16 @@
+import React from "react";
+import MainLayout from "../layouts/MainLayout";
+import '../assets/css/about.css'
+
+const NotFound = () => {
+    return(
+        <MainLayout>
+            <div className="about-container">
+                <h1>404 not found page</h1>
+                
+            </div>
+        </MainLayout>
+    )
+}
+
+export default NotFound
